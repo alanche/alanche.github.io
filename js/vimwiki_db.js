237590704ba3c9518abcb7a072fcf289a89b0bc2
alanche <alan-che@qq.com>
2017-05-15 22:25:42 +0800

    add new pages

--- js/vimwiki_db.js
+++ js/vimwiki_db.js
@@ -14,7 +14,8 @@ var vimwiki =
             "ARM"
         ],
         "TOOLS": [
-            "GIT"
+            "GIT",
+            "EDA"
         ],
         "Programming": [
             "SystemVerilog",
@@ -28,7 +29,8 @@ var vimwiki =
             "UsefulLinks",
             "VIM\u652f\u6301\u975e\u7b49\u5bbd\u4e2d\u6587\u5b57\u4f53",
             "Excel-VBA_\u8f6c\u6362\u4eba\u6c11\u5e01\u4e3a\u5927\u5199",
-            "\u89e3\u51b316K\u622a\u56fe\u8f6f\u4ef6\u9ed1\u5c4f\u95ee\u9898"
+            "\u89e3\u51b316K\u622a\u56fe\u8f6f\u4ef6\u9ed1\u5c4f\u95ee\u9898",
+            "VPN\u8fde\u63a5\u65e0\u6cd5\u8bbf\u95ee\u4e92\u8054\u7f51\u7684\u89e3\u51b3\u529e\u6cd5"
         ],
         "About": [
             "ME"
